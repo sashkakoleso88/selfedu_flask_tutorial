97bfc7ea18c4aaf35736b7f8f5bb0914b22383e6
sashkakoleso88 <sashkakoleso88@gmail.com>
2023-01-28 17:11:39 +0200

    lesson 2. Using templates of web pages

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+menu = ["Install", "Tutorial", "Contacts"]
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'index'
+    return render_template('index.html', menu=menu)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About us</h1>'
+    return render_template('about.html', title='About us', menu=menu)
 
 
 if __name__ == '__main__':
